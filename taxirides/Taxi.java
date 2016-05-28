ec90330dba8bd4877c4f696cae7e167f04503a4f
TheLuckyLucker <conni.blach@web.de>
2016-05-28 22:35:57 +0200

    delete spatie

--- taxirides/Taxi.java
+++ taxirides/Taxi.java
@@ -1,6 +1,7 @@
 package taxirides;
+
 /**
- * 
+ *
  * @author Tiko Huizinga - s44608988
  * @author Conny Blach - s4329872
  *
@@ -16,7 +17,7 @@ public class Taxi {
     private final Station station;
     private int totalNrOfPassengers = 0;
     private int nrOfRides = 0;
-  
+
     public Taxi(int nr, int maxNumberOfPassengers, int transportationTime, Station station) {
         this.taxiId = nr;
         this.maxNrOfPassengers = maxNumberOfPassengers;
@@ -26,34 +27,34 @@ public class Taxi {
     }
 
     /**
-     * Tries to take maxNrOfPassenegers from the station. If actual number is less then that number is taken
+     * Tries to take maxNrOfPassenegers from the station. If actual number is
+     * less then that number is taken
      */
     public void takePassengers() {
-        int passengersWaiting = station.getNrOfPassengersWaiting();        
-        if ( passengersWaiting > 0 ) {
-            int nrOfPassengers = Math.min(passengersWaiting, maxNrOfPassengers);
-            station.leaveStation(nrOfPassengers);
-            totalNrOfPassengers += nrOfPassengers;
+        int passengersTaken = station.tryPassengers(maxNrOfPassengers);
+        if (passengersTaken > 0) {
+            totalNrOfPassengers += passengersTaken;
             nrOfRides++;
-            System.out.println("Taxi " + taxiId + " takes " + nrOfPassengers + " passengers");
+            System.out.println("Taxi " + taxiId + " takes " + passengersTaken + " passengers");
         } else {
-            System.out.println("Taxi " + taxiId + " takes no passengers");  
+            System.out.println("Taxi " + taxiId + " takes no passengers");
             try {
-              TimeUnit.MILLISECONDS.sleep(SLEEPTIME); // if no passengers at the station wait some time
-            }
-            catch (InterruptedException e) {
-              e.printStackTrace();
+                TimeUnit.MILLISECONDS.sleep(SLEEPTIME); // if no passengers at the station wait some time
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
+
     /**
-     * Calculates the total time of this taxi by multiplying the number of rides by the transportation time
+     * Calculates the total time of this taxi by multiplying the number of rides
+     * by the transportation time
+     *
      * @return total time
      */
     public int calcTotalTime() {
         return transportationTime * nrOfRides;
     }
-    
 
     public int getTotalNrOfPassengers() {
         return totalNrOfPassengers;
